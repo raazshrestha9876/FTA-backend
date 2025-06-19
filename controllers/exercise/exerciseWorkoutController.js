16516ac00d9fe053f1e48317221d02c07dfa73f9
raazshrestha9876 <raazshrestha9876@gmail.com>
2025-06-19 18:37:06 +0545

    message

--- controllers/exercise/exerciseWorkoutController.js
+++ controllers/exercise/exerciseWorkoutController.js
@@ -22,25 +22,78 @@ export const stopWorkout = async (req, res) => {
   }
 };
 
-export const getTotalCalories = async (req, res) => {
+export const userCaloriesStatsAnalytics = async (req, res) => {
   try {
     const results = await ExerciseWorkout.aggregate([
-      { $match: { user: new mongoose.Types.ObjectId(req.user) }},
+      { $match: { user: new mongoose.Types.ObjectId(req.user) } },
+      {
+        $facet: {
+          dailyCalories: [
+            {
+              $group: {
+                _id: {
+                  $dateToString: { format: "%Y-%m-%d", date: "$created_at" },
+                },
+                totalDailyCalories: { $sum: "$caloriesBurned" },
+              },
+            },
+            { $sort: { _id: 1 } },
+            {
+              $project: {
+                _id: 0,
+                date: "$_id",
+                totalDailyCalories: 1,
+              },
+            },
+          ],
+          totalCalories: [
+            {
+              $group: {
+                _id: "$user",
+                totalCalories: { $sum: "$caloriesBurned" },
+              },
+            },
+            {
+              $project: {
+                _id: 0,
+                user: "$_id",
+                totalCalories: 1,
+              },
+            },
+          ],
+        },
+      },
+    ]);
+
+    const dailyCalories = results[0].dailyCalories;
+    const totalCalories = results[0].totalCalories[0] || { totalCalories: 0 };
+
+    res.status(200).json({
+      dailyCalories,
+      totalCalories,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const totalCaloriesBurnedAllUserStats = async (req, res) => {
+  try {
+    const results = ExerciseWorkout.aggregate([
       {
         $group: {
-          _id: "$user",
+          _id: null,
           totalCalories: { $sum: "$caloriesBurned" },
         },
       },
       {
         $project: {
           _id: 0,
-          user: "$_id",
           totalCalories: 1,
         },
       },
     ]);
-    res.status(200).json(results);
+    res.status(200).json(results[0] || { totalCalories: 0 });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- routes/exercise/exerciseWorkoutRoutes.js
+++ routes/exercise/exerciseWorkoutRoutes.js
@@ -1,15 +1,18 @@
 import {
   stopWorkout,
   startWorkout,
-  getTotalCalories,
+  userCaloriesStatsAnalytics,
+  totalCaloriesBurnedAllUserStats,
 } from "../../controllers/exercise/exerciseWorkoutController.js";
 import express from "express";
 import { protect } from "../../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/get-total-calories", protect, getTotalCalories);
+router.get("/get-total-user-calories", protect,  userCaloriesStatsAnalytics);
+router.get("/get-total-all-user-calories", totalCaloriesBurnedAllUserStats);
 router.post("/start", protect, startWorkout);
 router.post("/stop/:id", protect, stopWorkout);
+
 
 export default router;
